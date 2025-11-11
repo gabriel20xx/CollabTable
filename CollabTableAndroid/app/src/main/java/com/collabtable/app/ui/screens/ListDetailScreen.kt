e98588b22852f3122e7801d4f413b744e98ca43c
gabriel20xx <gabrielfranz31@gmail.com>
2025-11-11 22:10:41 +0100

    feat: adjust ItemRow height to minimum intrinsic size for better layout consistency

--- CollabTableAndroid/app/src/main/java/com/collabtable/app/ui/screens/ListDetailScreen.kt
+++ CollabTableAndroid/app/src/main/java/com/collabtable/app/ui/screens/ListDetailScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -919,6 +920,7 @@ fun ItemRow(
         modifier =
             Modifier
                 .fillMaxWidth()
+                .height(IntrinsicSize.Min)
                 .clickable(onClick = onClick)
                 ,
         verticalAlignment = Alignment.CenterVertically,
