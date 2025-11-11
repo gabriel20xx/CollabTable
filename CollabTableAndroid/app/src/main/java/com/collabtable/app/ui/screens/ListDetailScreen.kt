1624da76b26e154bd1813dde8c0886dedc840dc4
gabriel20xx <gabrielfranz31@gmail.com>
2025-11-11 22:13:39 +0100

    fix: specify IntrinsicSize from layout package for ItemRow height

--- CollabTableAndroid/app/src/main/java/com/collabtable/app/ui/screens/ListDetailScreen.kt
+++ CollabTableAndroid/app/src/main/java/com/collabtable/app/ui/screens/ListDetailScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -920,7 +919,7 @@ fun ItemRow(
         modifier =
             Modifier
                 .fillMaxWidth()
-                .height(IntrinsicSize.Min)
+                .height(androidx.compose.foundation.layout.IntrinsicSize.Min)
                 .clickable(onClick = onClick)
                 ,
         verticalAlignment = Alignment.CenterVertically,
